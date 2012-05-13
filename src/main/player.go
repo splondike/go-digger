3a22fcc006e1f8e9052f4a5b475667ec9aee8a76
Stefan Schneider-Kennedy <code@stefansk.name>
2012-05-13 16:06:18 +1000

    Added logging function

--- src/main/player.go
+++ src/main/player.go
@@ -26,6 +26,19 @@ func main() {
       }
    }
 
+   log := func (msg string) {}
+   if len(os.Args) >= 6 {
+      file, err := os.Create(os.Args[5])
+      if err == nil {
+         log = func (msg string) {
+            file.WriteString(msg)
+         }
+      } else {
+         println("Couldn't open file [" + os.Args[5] + "] for writing.")
+         os.Exit(2)
+      }
+   }
+
    player := NewPlayer()
 
    actionPipe := make(chan Action, 100)
@@ -40,6 +53,6 @@ func main() {
    for {
       move := <-actionPipe
       move.Do(pa, player.CurrentWorld)
-      //println(move.String(), player.CurrentWorld.String())
+      log(move.String() + "\n")
    }
 }
